2e20a35aab4e4d1b9352e0cd27df2c30f18adcc6
ATILLAv2 <102419938+ATILLAv2@users.noreply.github.com>
2022-05-26 13:33:08 +0500

    Add files via upload

--- php/order.php
+++ php/order.php
@@ -9,25 +9,23 @@
 <body>
     <?php
     $conn = mysqli_connect("chuc.caseum.ru:33333", "st_2_18_28", "63567181", "is_2_18_st28_VKR");
-
     $name = $_POST["fio"];
     $phone = $_POST["phoneNumber"];
-    /* echo $name;
-    echo $phone; */
+    $sum = $_POST["price"];
+    $date = $_POST["date"];
 
     $zak = "INSERT INTO t_Client (fio, phone) VALUES ('$name', '$phone')";
     $conn->query($zak);
     $client_id = mysqli_insert_id($conn);
-    /* echo $client_id; */
-    $mainOeder = "INSERT INTO t_OrderMain (client_id) VALUES (".$client_id.")";
-    $conn->query($mainOeder);
-    $mainOeder_id = mysqli_insert_id($conn);
+    
+    $mainOrder = "INSERT INTO t_OrderMain (client_id, totalSumm, date) VALUES (".$client_id.", '$sum', '$date')";
+    $conn->query($mainOrder);
+    $mainOrder_id = mysqli_insert_id($conn);
 
-    /* $last = "SELECT idOrder FROM t_order WHERE (idOrder = LAST_INSERT_ID());";*/
     if(isset($_POST['form_2'])){
         $form = $_POST['form_2'];
         foreach($form as $item){
-            $Slave = "INSERT INTO t_OrderSlave (vid_rabot, mainOrder_id) VALUES ('".$item."', ".$mainOeder_id.")";
+            $Slave = "INSERT INTO t_OrderSlave (vid_rabot, mainOrder_id) VALUES ('".$item."', ".$mainOrder_id.")";
             $conn->query($Slave);
         };   
     }
